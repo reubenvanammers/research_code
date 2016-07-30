c710b60c8c6d60107c93e4aa84a93543ac78f0a8
Reuben van Ammers <reubenvanammers@gmail.com>
2016-07-30 16:42:52 +1000

    2nd commit

---
+++ Code/research proj matlab/columnize.m
@@ -0,0 +1,4 @@
+function C = columnize([R,P])
+R = R(:);
+P = P(:);
+C = [R;P];
---
+++ Code/research proj matlab/matricize.m
@@ -0,0 +1,8 @@
+function [R,P] =  matricize(column)
+%converts columnn of x,y data to xy coordinates in 2d matrix
+N = length(column);
+N = N/2;
+real_column = column(1:N);
+reference_column = column(N+1:2*N);
+R = reshape(real_column,[],2);
+P = reshape(reference_column,[],2);
