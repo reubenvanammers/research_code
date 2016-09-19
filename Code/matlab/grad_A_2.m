d5507381503b25071ff98a0f491a9cb01ebc50df
Reuben van Ammers <reubenvanammers@gmail.com>
2016-09-19 21:46:24 +1000

    some optimizations

--- Code/matlab/grad_A_2.m
+++ Code/matlab/grad_A_2.m
@@ -1,8 +1,13 @@
 function A_vec = grad_A_2(i,l,C,V)
 %implements gradient of area for area based force in vertex model
-j = C{l}==i;
-jp1 = circshift(j,1,2);
-jm1 = circshift(j,-1,2);
-vp1 = V(C{l}(jp1),:);
-vm1 = V(C{l}(jm1),:);
+% j = C{l}==i;
+% jp1 = circshift(j,1,2);
+% jm1 = circshift(j,-1,2);
+len = length(C{l});
+internal_vertex_number = find(i==C{l})-1;
+vp1 = V(C{l}(mod(internal_vertex_number + 1,len)+1),:);
+vm1 = V(C{l}(mod(internal_vertex_number - 1,len)+1),:);
+
+% vp1 = V(C{l}(jp1),:);
+% vm1 = V(C{l}(jm1),:);
 A_vec = 0.5*[vp1(2)-vm1(2);vm1(1)-vp1(1)]';
--- Code/matlab/vertex_internal_force_calc.m
+++ Code/matlab/vertex_internal_force_calc.m
@@ -1,4 +1,4 @@
-function vertex_force = vertex_internal_force_calc(C,V,included_cell,lambda,beta,gamma,A_target,C_target)
+function vertex_force = vertex_internal_force_calc(C,V,included_cell,lambda,beta,gamma,A_target,A_current,C_target,C_current)
 %calculates the internal forces felt by vertices on connectivity and target
 %parameters. Used for both real and reference cells. 
 %A_target(i),C_target(i) should be vectors for each cell
@@ -10,8 +10,8 @@ for  i= 1:N;%i is vertex number
         gradilm1 = grad_d_3(i,l,-1,C,V);
         gradil0 = grad_d_3(i,l,0,C,V);
         vertex_force(i,:) = vertex_force(i,:) +...
-            2*lambda*(cell_area(l,C,V)-A_target(l))*grad_A_2(i,l,C,V)+...
-            2*beta*(cell_circumference(l,C,V)-C_target(l))*(gradilm1-gradil0)+...
+            2*lambda*(A_current(l)-A_target(l))*grad_A_2(i,l,C,V)+...
+            2*beta*(C_current(l)-C_target(l))*(gradilm1-gradil0)+...
             gamma*gradilm1-gamma*gradil0;%due to definition subtract grad_d_3 instead of add
     end
 end
